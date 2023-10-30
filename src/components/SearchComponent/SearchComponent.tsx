c32e238736cceb57c4a92f59d5293a0dfec25047
DartSter <kil-all@rambler.ru>
2023-10-30 13:25:21 +0200

    feat: add in search component functions for get and set search term in local stor, and work with input

--- src/components/SearchComponent/SearchComponent.tsx
+++ src/components/SearchComponent/SearchComponent.tsx
@@ -1,8 +1,29 @@
-import { Component } from 'react';
+import { ChangeEvent, Component } from 'react';
 import SearchString from '../SearchString/SearchString';
-import './searchComponent.css';
 import SearchResult from '../SearchResult/SearchResult';
+import './searchComponent.css';
 export default class SearchComponent extends Component {
+  state = {
+    searchInput: '',
+    searchResults: [],
+  };
+
+  componentDidMount() {
+    const savedSearchInput = localStorage.getItem('searchInput');
+    if (savedSearchInput) {
+      this.setState({ searchInput: savedSearchInput });
+      this.handleSearchClick();
+    }
+  }
+
+  handleSearchClick = () => {
+    localStorage.setItem('searchInput', this.state.searchInput);
+  };
+
+  handleSearchInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchInput: event.target.value });
+  };
+
   render() {
     return (
       <div className="search-wrapper">
