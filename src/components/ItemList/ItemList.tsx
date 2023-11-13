3559059275772fae8041dc5c09f1711257da0eab
DartSter <kil-all@rambler.ru>
2023-11-13 20:09:13 +0200

    refactor: item list converting a class component to a functional one

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,24 +1,22 @@
-import { Component } from 'react';
 import { IPokemonItem } from '../../utils/fetchPokemonList';
 import './ItemList.css';
-export default class ItemList extends Component<{
-  listData: IPokemonItem[] | never[];
-}> {
-  render() {
-    return (
-      <div className="list">
-        <h2 className="list-header">Pokemon List</h2>
-        <div className="list-field">
-          {this.props.listData &&
-            this.props.listData.length > 0 &&
-            this.props.listData.map((item) => (
-              <div key={item.name} className="list-item">
-                <div className="list-name">Name: {item.name}</div>
-                <div>URL: {item.url}</div>
-              </div>
-            ))}
-        </div>
+
+const ItemList = ({ listData }: { listData: IPokemonItem[] | never[] }) => {
+  return (
+    <div className="list">
+      <h2 className="list-header">Pokemon List</h2>
+      <div className="list-field">
+        {listData &&
+          listData.length > 0 &&
+          listData.map((item: IPokemonItem) => (
+            <div key={item.name} className="list-item">
+              <div className="list-name">Name: {item.name}</div>
+              <div>URL: {item.url}</div>
+            </div>
+          ))}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default ItemList;
